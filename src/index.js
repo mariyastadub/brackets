60ef2a14b5d7fefb6d683ad053bc31385bf96f93
Mariya Stadub <mariya_lunkina@mail.ru>
2019-03-24 22:11:21 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const opening = { '(': ')', '[': ']', '{': '}','1':'2','3':'4','5':'6','7':'7','8':'8'}
+  const closing = { ')': '(', ']': '[', '}': '{','2':'1','4':'3','6':'5','7':'7','8':'8'}
+
+  const stack = [];
+
+  for (c of str.split('')) {
+    if (c in opening)
+      stack.push(c)
+    else if (stack.pop() !== closing[c])
+      return false
+  }
+
+  return (stack.length === 0)
 }
